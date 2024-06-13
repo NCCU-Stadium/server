2e36630191bc12810872dc8fbffee82e417b4b84
GINTOAHC <chaotingchen10@gmail.com>
2024-06-13 20:41:06 +0800

    style

--- src/database/shops/products/delete.ts
+++ src/database/shops/products/delete.ts
@@ -1,9 +1,15 @@
 import { query } from '../../database'
 
 export const deleteProduct = async (product_id: string) => {
-    await query('DELETE FROM product_t WHERE id = $1', [product_id]);
-};
+  await query('DELETE FROM product_t WHERE id = $1', [product_id])
+}
 
-export const deleteProductInfo = async (product_id: string, product_store_id: string) => {
-    await query('DELETE FROM productStore_t WHERE product_id = $1 AND id = $2', [product_id, product_store_id]);
-};
+export const deleteProductInfo = async (
+  product_id: string,
+  product_store_id: string
+) => {
+  await query('DELETE FROM productStore_t WHERE product_id = $1 AND id = $2', [
+    product_id,
+    product_store_id,
+  ])
+}
--- src/database/shops/products/post.ts
+++ src/database/shops/products/post.ts
@@ -1,18 +1,29 @@
 import { query } from '../../database'
 
-export const createProduct = async (name: string, brand: string, price: number, desc: string, imgurl: string[]) => {
-    const result = await query(
-        'INSERT INTO product_t (name, brand, price, desc, imgurl) VALUES ($1, $2, $3, $4, ARRAY $5) RETURNING id',
-        [name, brand, String(price), desc, imgurl]
-    );
-    return result.rows[0].id;
-};
+export const createProduct = async (
+  name: string,
+  brand: string,
+  price: number,
+  desc: string,
+  imgurl: string[]
+) => {
+  const result = await query(
+    'INSERT INTO product_t (name, brand, price, desc, imgurl) VALUES ($1, $2, $3, $4, ARRAY $5) RETURNING id',
+    [name, brand, String(price), desc, imgurl]
+  )
+  return result.rows[0].id
+}
 
-export const addProductInfo = async (product_id: string, size: string, color: string, sold: number, count: number) => {
-    const result = await query(
-        'INSERT INTO productStore_t (product_id, size, color, count, sold) VALUES ($1, $2, $3, $4, $5) RETURNING id',
-        [product_id, size, color, String(count), String(sold)]
-    );
-    return result.rows[0].id;
-};
-
+export const addProductInfo = async (
+  product_id: string,
+  size: string,
+  color: string,
+  sold: number,
+  count: number
+) => {
+  const result = await query(
+    'INSERT INTO productStore_t (product_id, size, color, count, sold) VALUES ($1, $2, $3, $4, $5) RETURNING id',
+    [product_id, size, color, String(count), String(sold)]
+  )
+  return result.rows[0].id
+}
