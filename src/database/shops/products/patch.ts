75456ff5695bb91be0dba3bea3c1106c97861a7f
Sebastian <kevin1111006@gmail.com>
2024-06-14 12:52:39 +0800

    Fix: patch

--- src/database/shops/products/patch.ts
+++ src/database/shops/products/patch.ts
@@ -1,10 +1,25 @@
 import { query } from '../../database'
 
+type updateProduct = {
+  name?: string
+  brand?: string
+  price?: number
+  desc?: string
+  imgurl?: string[]
+}
+
 export const updateProduct = async (product_id: string, updates: any) => {
+  const { decoded, ...temp } = updates
+  updates = temp
   const fields = Object.keys(updates)
-    .map((key, idx) => `${key} = $${idx + 2}`)
-    .join(', ')
+    .map((key, idx) => {
+      if(key=='desc') return `"${key}" = $${idx + 2}`
+      else if(key=='imgurl') return `${key} = ARRAY[$${idx + 2}]`
+      else return `${key} = $${idx + 2}`
+    })  
+    .join(', ') 
   const values = Object.values(updates)
+  // console.error(fields)
   await query(`UPDATE product_t SET ${fields} WHERE id = $1`, [
     product_id,
     ...values,
@@ -16,6 +31,8 @@ export const updateProductInfo = async (
   product_store_id: string,
   updates: any
 ) => {
+  const { decoded, ...temp } = updates
+  updates = temp
   const fields = Object.keys(updates)
     .map((key, idx) => `${key} = $${idx + 3}`)
     .join(', ')
