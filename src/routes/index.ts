d4bd262aab1e8f50c4e45b86b74ab208ce298102
wanchichang <a0965266902@gmail.com>
2024-06-14 11:22:22 +0800

    feat: add users/reserves routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,3 @@
 export { default as authRouter } from './auth'
 export { default as activitiesRouter } from './activities'
-export { default as usersRouter } from './users'
-export { default as reservesRouter } from './users/reserves'
-
+export { default as usersRouter } from './users' 
