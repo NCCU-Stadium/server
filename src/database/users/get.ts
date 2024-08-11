b8f139d074d33a522144555c23c17671ec782413
GNITOAHC <chaotingchen10@gmail.com>
2024-08-11 22:07:46 +0800

    Feat/subuser (#18)
    * feat: Create/Delete subuser
    
    * feat: Auto create subuser for registration
    
    * feat: get subuser
    
    * docs: update get route
    
    * style
    
    ---------
    
    Co-authored-by: wanchichang <a0965266902@gmail.com>

--- src/database/users/get.ts
+++ src/database/users/get.ts
@@ -21,3 +21,32 @@ export async function hasSubuser(mail: string, name: string): Promise<boolean> {
   }
   return true
 }
+export async function getSubuser(mail: string) {
+  const qstring = 'select name, avatar from subuser_t where user_mail = $1'
+  const res = await query(qstring, [mail])
+  if (res.rowCount === 0) {
+    return { error: 'subUser not found' }
+  }
+  return { arr: res.rows }
+
+  // return {
+  //   mail: res.rows[0].mail,
+  //   pass: res.rows[0].pass,
+  //   role: res.rows[0].role,
+  // }
+}
+
+export async function getSubuserInfo(mail: string, name: string) {
+  const qstring = 'select * from subuser_t where user_mail = $1 and name = $2'
+  const res = await query(qstring, [mail, name])
+  if (res.rowCount === 0) {
+    return { error: 'subUser not found' }
+  }
+  return { arr: res.rows }
+
+  // return {
+  //   mail: res.rows[0].mail,
+  //   pass: res.rows[0].pass,
+  //   role: res.rows[0].role,
+  // }
+}
