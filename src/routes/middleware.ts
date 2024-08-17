2206014ba30de48861744104eddc123b40df9623
GINTOAHC <chaotingchen10@gmail.com>
2024-08-18 02:07:27 +0800

    feat: adminProtect middleware

--- src/routes/middleware.ts
+++ src/routes/middleware.ts
@@ -3,6 +3,12 @@ import express from 'express'
 import { env } from '../constants'
 import { ErrorRequestHandler } from 'express'
 
+export type DecodedType = {
+  mail: string
+  role: string
+  iat: number
+}
+
 export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
   console.error(err.stack)
   res.status(500).send('Internal Server Error')
@@ -20,7 +26,7 @@ export function jwtProtect(
   const token = authHeader.split(' ')[1]
   jwt.verify(token, env.JWT_SECRET as string, (err, user) => {
     if (!err) {
-      req.body.decoded = user
+      req.body.decoded = user as DecodedType
       return next()
     }
     if (err.name === 'TokenExpiredError') {
@@ -31,4 +37,19 @@ export function jwtProtect(
     }
     return
   })
+}
+
+export function adminProtect(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  const decoded = req.body.decoded
+  if (!decoded || !decoded.role) {
+    return res.status(403).send('Only admin can access')
+  }
+  if (decoded.role !== 'admin') {
+    return res.status(403).send('Only admin can access')
+  }
+  return next()
 }
